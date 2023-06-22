96538b0d3b6393ae6fd45e4ca587a177fc5af86f
Art96em <artem-w0lf@yandex.ru>
2023-06-22 14:26:30 +0300

    Tests in react

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,20 @@
-import React from 'react';
+import React, { useRef } from 'react';
 import { render, screen } from '@testing-library/react';
 import App from './App';
+import { matrixSum, range } from './util/number-functions';
+import LifeMatrix from './service/LifeMatrix';
+import lifeConfig from "./config/live-game-config.json"
+const { dimension, tick } = lifeConfig;
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
-});
+test("sum of matrix", () => {
+  expect(matrixSum([[1, 2, 3], [4, 5, 6]])).toEqual(21)
+})
+
+test("range test", () => {
+  const lifeMatrix = new LifeMatrix(lifeConfig.example1)
+  lifeMatrix.next()
+  
+  expect(lifeMatrix.numbers).toEqual([[0, 0, 0], [1, 1, 1], [0, 0, 0]])
+  lifeMatrix.next()
+  expect(lifeMatrix.numbers).toEqual([[0, 1, 0], [0, 1, 0], [0, 1, 0]])
+})
