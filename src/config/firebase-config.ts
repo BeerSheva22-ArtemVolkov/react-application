71627c3760e3b77eb70ca28d806223f867e4ee91
Art96em <artem-w0lf@yandex.ru>
2023-07-11 17:13:20 +0300

    firebase config

---
+++ src/config/firebase-config.ts
@@ -0,0 +1,23 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getDatabase } from "firebase/database";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCYX-6JVxbB1lFXh5_FvlLj_gc1lG0YfYo",
+    authDomain: "employees-art96.firebaseapp.com",
+    projectId: "employees-art96",
+    storageBucket: "employees-art96.appspot.com",
+    messagingSenderId: "1006116516210",
+    appId: "1:1006116516210:web:2a050fcf9019483a3c6deb"
+};
+
+// Initialize Firebase
+const appFirebase = initializeApp(firebaseConfig);
+
+// Initialize Realtime Database and get a reference to the service
+// const database = getDatabase(appFirebase);
+
+export default appFirebase
